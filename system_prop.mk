e2b49d6435c5e0f4af21bfe8cf4b7b33e2e44790
blacklytning <65107343+blacklytning@users.noreply.github.com>
2020-09-23 09:04:33 +0530

    RMX1911 : Add surface flinger prop
    Add surface flinger prop to eliminate ui stutters and lag.

--- system_prop.mk
+++ system_prop.mk
@@ -36,7 +36,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
     debug.sf.enable_gl_backpressure=1 \
-    debug.sf.enable_hwc_vds=1
+    debug.sf.enable_hwc_vds=1 \
+    debug.sf.latch_unsignaled=1
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.vendor.display.enable_kernel_idle_timer=true \
