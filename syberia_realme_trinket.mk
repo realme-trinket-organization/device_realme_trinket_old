5ccc6137b357404097086581b1ca4c606b15d478
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2020-10-09 21:24:15 +0700

    realme_trinket: Init Syberia

--- syberia_realme_trinket.mk
+++ syberia_realme_trinket.mk
@@ -6,22 +6,16 @@
 
 $(call inherit-product, device/realme/realme_trinket/device.mk)
 
-# Inherit some common Aosip stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+# Inherit some common Syberia stuff.
+$(call inherit-product, vendor/syberia/config/common_full_phone.mk)
 
-# Setup Gapps options
-IS_PHONE := true
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_MINIMAL_APPS := false
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := realme_trinket
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := derp_realme_trinket
+PRODUCT_NAME := syberia_realme_trinket
 
 BUILD_FINGERPRINT := "trinket-user-10-QKQ1.200209.002--release-keys"
 
